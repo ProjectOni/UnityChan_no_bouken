793672b31967cc3c995c2dcf245265580edbdbd1
KentoYamamoto <g2114034@fun.ac.jp>
2015-05-24 02:10:21 +0900

    create title

---
+++ Assets/script/CreateTitleButton.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateTitleButton : MonoBehaviour {
+	static public bool isClient = true;
+	static public string ip = "127.0.0.1";
+	static public string port = "1192";
+
+	public void OnGUI()
+	{
+		GUI.Label (new Rect(200, 20, 500, 100), "UnityChanのおにごっこ");
+		GUI.Label (new Rect(200, 45, 100, 25), "ip address");
+		ip = GUI.TextField (new Rect(200, 70, 100, 25), ip);
+		GUI.Label (new Rect(300, 45, 100, 25), "port number");
+		port = GUI.TextField (new Rect(300, 70, 100, 25), port);
+		if (GUI.Button (new Rect (10, 10, 90, 90), "Client")) {
+			isClient = true;
+			Application.LoadLevel (1);
+		}
+		if (GUI.Button (new Rect (10, 110, 90, 90), "Master")) {
+			isClient = false;
+			Application.LoadLevel (1);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/script/NetworkManager.cs
+++ Assets/script/NetworkManager.cs
@@ -3,49 +3,36 @@ using System.Collections;
 
 public class NetworkManager : MonoBehaviour {
 	public GameObject objectPrefab;
-	public static Object unitychan;
 	string ip = "127.0.0.1";
 	string port = "1192";
 	bool connected = false;
 	
 	private void CreatePlayer()
 	{
-		//DontDestroyOnLoad(objectPrefab);
-		connected = true;
-		Application.LoadLevel (1);
-		//DontDestroyOnLoad(Network.Instantiate(objectPrefab, objectPrefab.transform.position, objectPrefab.transform.rotation, 1));
-		unitychan = Network.Instantiate(objectPrefab, objectPrefab.transform.position, objectPrefab.transform.rotation, 1); 
-		DontDestroyOnLoad (unitychan);
+		Network.Instantiate(objectPrefab, objectPrefab.transform.position, objectPrefab.transform.rotation, 1); 
 	}
-	
+
 	public void OnConnectedToServer()
-	{
-		CreatePlayer();
+	{    
+		connected = true;
+		CreatePlayer ();
 	}
 	
 	public void OnServerInitialized()
 	{
-		CreatePlayer();
-	}
-	
-	
-	public void OnGUI()
-	{
-		if( !connected)
-		{
-			if( GUI.Button( new Rect( 10, 10, 90, 90), "Client"))
-			{    
-				Network.Connect(ip, int.Parse(port) ); 
-			}
-			if( GUI.Button( new Rect(10, 110, 90, 90), "Master"))
-			{    
-				Network.InitializeServer(10, int.Parse(port), false);    
-			}
-		}
+		connected = true;
+		CreatePlayer ();
 	}
 
+
 	void Start () {
-		DontDestroyOnLoad (this);
+		if (CreateTitleButton.isClient) {
+			Network.Connect (ip, int.Parse (port));
+		} else {
+			Network.InitializeServer (10, int.Parse (port), false);
+		}
 	}
 	void Update () {}
+
+
 }
