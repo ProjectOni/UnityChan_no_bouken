10ef7313eefeea1235c703e3ddec16f93a98f98c
Masahiro Nagata <g2114023@fun.ac.jp>
2015-05-30 01:09:51 +0900

    アイテムを取ったら装備できるようにした

---
+++ Assets/Resources/script/GetItem.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetItem : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider collider){
+		if (collider.tag == "Player") {
+			if(!unityChanState.IsEquipRight()){
+				unityChanState.SetIsEquipRight(true);
+			}
+			unityChanState.SetWeaponNameRight(this.gameObject.name);
+			Destroy(this.gameObject);
+			return;
+		}
+
+	}
+
+}
